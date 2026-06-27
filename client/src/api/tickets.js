e645d44d5643fb36a48a8942909f015699aa31c8
Lalitesh Kannuri <kannuri.lalitesh@gmail.com>
2026-06-27 08:31:26 +0530

    feat: update API base URL to use environment variable

--- client/src/api/tickets.js
+++ client/src/api/tickets.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE = 'http://localhost:5000/api'
+const BASE = import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 
 export const createTicket = (data) => axios.post(`${BASE}/tickets`, data)
 export const getTickets = () => axios.get(`${BASE}/tickets`)
